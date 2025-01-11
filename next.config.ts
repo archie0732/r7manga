78df9bd05c3112a5080f317b7421be5ca4991138
水原千鶴 <121162902+archie0732@users.noreply.github.com>
2025-01-11 18:23:42 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,32 +2,33 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
+    domains: ['t2.nhentai.net'], // 如果您想使用较简单的配置方式
     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 't2.nhentai.net',
+        port: '',
+        pathname: '/galleries/**/**',
+      },
       {
         protocol: 'https',
         hostname: 't3.nhentai.net',
         port: '',
-        pathname: '/galleries/**',
-        search: '',
-      }, {
+        pathname: '/galleries/**/**',
+      },
+      {
         protocol: 'https',
         hostname: 'i3.nhentai.net',
         port: '',
-        pathname: '/galleries/**',
-        search: '',
-      }, {
+        pathname: '/galleries/**/**',
+      },
+      {
         protocol: 'https',
         hostname: 't4.qy0.ru',
         port: '',
         pathname: '/**',
-        search: '',
-      }, {
-        protocol: 'https',
-        hostname: 't4.qy0.ru',
-        port: '',
-        pathname: '/**',
-        search: '',
-      }],
+      },
+    ],
   },
 };
 
